b84c2bf906c04aadaf73e8f503136b08bbae5464
yuto <yuutonishie@gmail.com>
2026-04-16 21:39:23 +0900

    fix: clean up config.py

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
-import os
-import os as _os
-_os.makedirs(_os.path.join(_os.path.abspath(_os.path.dirname(__file__)), 'instance'), exist_ok=True)
+﻿import os
 
-# config.py が置いてあるフォルダ（PictorialBookWebApp）の絶対パスを取得
 basedir = os.path.abspath(os.path.dirname(__file__))
 instance_dir = os.path.join(basedir, 'instance')
 os.makedirs(instance_dir, exist_ok=True)
+
+DEBUG = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(instance_dir, 'db.sqlite')
-
-DEBUG = True
-
-# 絶対パスを使ってデータベースの場所を指定
-# これにより C:\Users\yuuto\Documents\PictorialBookWebApp\instance\db.sqlite を指すようになります
-db_path = os.environ.get('DATABASE_PATH', os.path.join(basedir, 'instance', 'db.sqlite'))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
-
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
