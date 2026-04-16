2b3e160d5510e8cb4103071dc7a2937a390335f1
yuto <yuutonishie@gmail.com>
2026-04-16 21:24:06 +0900

    yuto

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
+import os as _os
+_os.makedirs(_os.path.join(_os.path.abspath(_os.path.dirname(__file__)), 'instance'), exist_ok=True)
 
 # config.py が置いてあるフォルダ（PictorialBookWebApp）の絶対パスを取得
 basedir = os.path.abspath(os.path.dirname(__file__))
+instance_dir = os.path.join(basedir, 'instance')
+os.makedirs(instance_dir, exist_ok=True)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(instance_dir, 'db.sqlite')
 
 DEBUG = True
 
